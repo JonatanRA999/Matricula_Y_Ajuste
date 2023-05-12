ca0e92e2c511420e9ecd8ab00b351e8bb0af3ee1
yeison8a <yeison.ochoa@udea.edu.co>
2023-05-12 12:55:59 -0500

    correccion seleccion de horarios

--- src/Components/VentanaComponent.jsx
+++ src/Components/VentanaComponent.jsx
@@ -1,8 +1,6 @@
 import React, { useState } from "react";
 
-
 export function PopupButton() {
-
   const [selectedOption, setSelectedOption] = useState(null);
 
   const openPopup = () => {
@@ -12,38 +10,42 @@ export function PopupButton() {
     const top = window.innerHeight / 2 - height / 2;
 
     setTimeout(() => {
-      // Hacer la solicitud a la API
-      fetch('https://matriculaajustesapi-santiagobedoyao.b4a.run/horarios/10009')
-        .then(response => response.json())
-        .then(data => {
+      fetch("https://matriculaajustesapi-santiagobedoyao.b4a.run/horarios/10009")
+        .then((response) => response.json())
+        .then((data) => {
+          const popup = window.open(
+            "",
+            "popup",
+            `width=${width}, height=${height}, left=${left}, top=${top}, center=yes`
+          );
 
-          //crear boton de acepta
-          const popup = window.open('', 'popup', `width=${width}, height=${height}, left=${left}, top=${top}, center=yes`);
-          const acceptButton = document.createElement('button');
-          acceptButton.textContent = 'Aceptar';
-          acceptButton.classList.add('accept-button');
-          // Crear la lista de opciones
-          const optionsList = document.createElement('select');
-          optionsList.classList.add('options-list');
-          const defaultOption = document.createElement('option');
-          defaultOption.textContent = 'Seleccionar';
+          const acceptButton = document.createElement("button");
+          acceptButton.textContent = "Aceptar";
+          acceptButton.classList.add("accept-button");
+          acceptButton.addEventListener("click", () => {
+            const selectedValue = optionsList.value;
+            setSelectedOption(selectedValue);
+            popup.close();
+          });
+
+          const optionsList = document.createElement("select");
+          optionsList.classList.add("options-list");
+          const defaultOption = document.createElement("option");
+          defaultOption.textContent = "Seleccionar";
           defaultOption.disabled = true;
           defaultOption.selected = true;
           optionsList.appendChild(defaultOption);
-          data.forEach(option => {
-            const listItem = document.createElement('option');
+
+          data.forEach((option) => {
+            const listItem = document.createElement("option");
             listItem.textContent = `Grupo: ${option.Grupo}, Código: ${option.Codigo}, Horario: ${option.Horario}, Cupos disponibles: ${option.CuposDisponibles}`;
-            acceptButton.addEventListener('click', () => {
-              
-              setSelectedOption(option.Horario);
-              popup.close();
-            });
-            popup.document.body.appendChild(optionsList);
-            popup.document.body.appendChild(acceptButton);
-           popup.resizeTo(width, height);
+            listItem.value = option.Horario;
             optionsList.appendChild(listItem);
           });
-          
+
+          popup.document.body.appendChild(optionsList);
+          popup.document.body.appendChild(acceptButton);
+          popup.resizeTo(width, height);
         });
     }, 0);
   };
@@ -55,4 +57,3 @@ export function PopupButton() {
     </div>
   );
 }
-
